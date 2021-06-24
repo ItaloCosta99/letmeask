6de018d29124b818e8c6e73f911767c8f4adedba
Italo Costa <italocostajobs@gmail.com>
2021-06-24 01:53:44 -0300

    feat: ✨ Lógica de Autentificação com Firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import { Button } from "./components/Button";
+import { Route, BrowserRouter } from 'react-router-dom';
+
+import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
+
+import { AuthContextProvider } from './contexts/AuthContext';
 
 function App() {
   return (
-    <>
-      <Button text="Botão 1"/>
-      <Button text="Botão 3"/>
-    </>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
